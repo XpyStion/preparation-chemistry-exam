1f4b3aa33805137e0aee766a47eb50454a080d14
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2024-10-05 20:55:33 +0300

    Add user model

--- app/models.py
+++ app/models.py
@@ -1,13 +1,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+GENDER = [
+    ('male', 'Male'),
+    ('female', 'Female'),
+    ('other', 'Other'),
+]
 
-class Role(models.Model):
-    name = models.CharField(max_length=50)
+ROLES = [
+    ('admin', 'Admin'),
+    ('teacher', 'Teacher'),
+    ('student', 'Student'),
+]
 
 
 class AuthUser(AbstractUser):
-    roles = models.ManyToManyField(Role, related_name='users')
-
     is_staff = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
+
+    full_name = models.CharField(max_length=25)
+    gender = models.CharField(max_length=6, choices=GENDER, default='other')
+    role = models.CharField(max_length=15, choices=ROLES, default='student')
+    birth_date = models.DateField()
+    country = models.CharField(max_length=15)
+    phone = models.CharField(max_length=11,)
+    telegram = models.CharField(max_length=50)
